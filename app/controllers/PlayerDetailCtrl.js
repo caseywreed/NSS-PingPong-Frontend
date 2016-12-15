ee4780da482f741a4ce314ea9895586367afca32
Casey Reed <casey.w.reed@gmail.com>
2016-12-15 10:06:14 -0600

    Player detail now filters from TopCtrl

--- app/controllers/PlayerDetailCtrl.js
+++ app/controllers/PlayerDetailCtrl.js
@@ -6,32 +6,25 @@ app.controller("PlayerDetailCtrl", function ($scope, $routeParams, $q, DataFacto
     $scope.colors = ['#BF0E00', '#016775', '#6FB100'];
     $scope.labels =["Wins", "Losses", "Avg. Point Diff", "Win %"];
 
-    $scope.data = []
+    $scope.graphData = []
 
     $scope.init = function () {
-        console.log("TopCtrl data in PlayerDetailCtrl", $scope.$parent.playerDataCache.string)
-        console.log("testVariable in PlayerDetailCtrl", $scope.$parent.testVariable)
-        $q.all([
-            DataFactory.getSinglePlayer($routeParams.playerId),
-            DataFactory.getAverageStats()
-            ])
-        .then(function (data    ) {
-            $scope.player = data[0].data;
-            let statsArray = [
-                data[0].data.stats.wins,
-                data[0].data.stats.losses,
-                data[0].data.stats.avgPointDiff,
-                data[0].data.stats.winPercentage
-            ]
-            $scope.data.push(statsArray)
-            let avgStatsArray = [
-                data[1].data.wins,
-                data[1].data.losses,
-                data[1].data.avgPointDiff,
-                data[1].data.winPercentage
-            ]
-            $scope.data.push(avgStatsArray)
+        $scope.player = $scope.dataCache.playerData.filter(function (p) {
+            return p.playerId == $routeParams.playerId
         })
+        $scope.player = $scope.player[0]
+        $scope.graphData.push([
+            $scope.player.stats.wins,
+            $scope.player.stats.losses,
+            $scope.player.stats.avgPointDiff,
+            $scope.player.stats.winPercentage
+        ])
+        $scope.graphData.push([
+            $scope.$parent.dataCache.avgPlayerStats.wins,
+            $scope.$parent.dataCache.avgPlayerStats.losses,
+            $scope.$parent.dataCache.avgPlayerStats.avgPointDiff,
+            $scope.$parent.dataCache.avgPlayerStats.winPercentage
+        ])
     }
 
 })
