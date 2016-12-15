871a32d644993ce43807eccebaf03c8b38a392d5
Casey Reed <casey.w.reed@gmail.com>
2016-12-15 16:04:33 -0600

    Controllers refactored so API call is in TopCtrl

--- app/controllers/TopCtrl.js
+++ app/controllers/TopCtrl.js
@@ -1,7 +1,20 @@
 "use strict";
 
-app.controller("TopCtrl", function ($scope) {
+app.controller("TopCtrl", function ($scope, $q, DataFactory) {
 
     $scope.dataCache = {}
+
+    $scope.loadDataFromAPI = function () {
+        console.log("Loading data from API")
+        $q.all([
+            DataFactory.getAllPlayers(),
+            DataFactory.getAverageStats()
+            ])
+        .then(function (data) {
+            $scope.dataCache.playerData = data[0].data
+            $scope.dataCache.avgPlayerStats = data[1].data
+        })
+    }
+
 
 })
